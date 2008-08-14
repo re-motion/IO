3f3d905529e65e2c08c4c901c1803b24a5bdf7b9
Matthias Goertler <matthias.goertler@rubicon.eu>
2008-08-14 11:01:44 +0000

    added build files for Dms.Tray changed Remotion.build

--- Dms/UnitTests/Properties/AssemblyInfoTemplate.cs
+++ Dms/UnitTests/Properties/AssemblyInfoTemplate.cs
@@ -14,11 +14,11 @@ using System.Reflection;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 //
-[assembly: AssemblyTitle ("Remotion Dms Library Unit Tests")]
-[assembly: AssemblyConfiguration(".NET Framework: net-3.5, build type: debug")]
-[assembly: AssemblyCompany("rubicon informationstechnologie gmbh")]
-[assembly: AssemblyProduct("Remotion")]
-[assembly: AssemblyCopyright("(c) 2005 - 2008 rubicon informationstechnologie gmbh, www.rubicon.eu")]
+[assembly: AssemblyTitle("Remotion Dms Library Unit Tests")]
+[assembly: AssemblyConfiguration("@buildinfo@")]
+[assembly: AssemblyCompany("@company@")]
+[assembly: AssemblyProduct("@productname@")]
+[assembly: AssemblyCopyright("@copyright@")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]		
 
@@ -33,4 +33,4 @@ using System.Reflection;
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
 
-[assembly: AssemblyVersion("1.11.0.2")]
+[assembly: AssemblyVersion("@versionnumber@")]
