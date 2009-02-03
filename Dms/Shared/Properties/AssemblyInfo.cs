5bdad6d9a2224e6fd8ab45d6af50419c7b05987f
Matthias Goertler <matthias.goertler@rubicon.eu>
2009-02-03 12:59:15 +0000

    Added Dms.Shared project (RM-1000)

---
+++ Dms/Shared/Properties/AssemblyInfo.cs
@@ -0,0 +1,46 @@
+// This file is part of re-vision (www.re-motion.org)
+// Copyright (C) 2005-2008 rubicon informationstechnologie gmbh, www.rubicon.eu
+// 
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License version 3.0 
+// as published by the Free Software Foundation.
+// 
+// This program is distributed in the hope that it will be useful, 
+// but WITHOUT ANY WARRANTY; without even the implied warranty of 
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the 
+// GNU Affero General Public License for more details.
+// 
+// You should have received a copy of the GNU Affero General Public License
+// along with this program; if not, see http://www.gnu.org/licenses.
+// 
+// Additional permissions are listed in the file re-motion_exceptions.txt.
+// 
+using System;
+using System.Reflection;
+
+//
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+//
+[assembly: AssemblyTitle ("Remotion Dms Shared Library")]
+[assembly: AssemblyDescription ("Classes and utilities used by Dms clients and server.")]
+[assembly: AssemblyConfiguration(".NET Framework: net-3.5, build type: debug")]
+[assembly: AssemblyCompany("rubicon informationstechnologie gmbh")]
+[assembly: AssemblyProduct("re-vision (www.re-motion.org)")]
+[assembly: AssemblyCopyright("(c) 2005 - 2008 rubicon informationstechnologie gmbh, www.rubicon.eu")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]		
+[assembly: CLSCompliant(true)]
+//
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers 
+// by using the '*' as shown below:
+
+[assembly: AssemblyVersion("1.11.19.2")]
