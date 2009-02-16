99653ac970c12ad1cc46fe9e246799bb7a9b505b
Matthias Goertler <matthias.goertler@rubicon.eu>
2009-02-16 16:12:00 +0000

    handle uploads via UploadRequestWrapper (inherits from WebRequestWrapper) added UserCanceledException to handle cancel while making request added some missing tests RM-1030

---
+++ Dms/Shared/Utilities/UserCanceledException.cs
@@ -0,0 +1,13 @@
+// Copyright (C) 2005 - 2008 rubicon informationstechnologie gmbh 
+// All rights reserved.
+//
+
+using System;
+
+namespace Remotion.Dms.Shared.Utilities
+{
+  public class UserCanceledException  : Exception
+  {
+    
+  }
+}
