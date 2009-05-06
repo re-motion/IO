8199cc352e80b493743458bde526a242508143e2
Tymon Terlikiewicz <tymon.terlikiewicz@rubicon.eu>
2009-05-06 12:20:46 +0000

    globalized some hardcoded strings added german globalization files added GlobalizationIndependentTestBase to enforce the "en-US" culture for all unit tests

---
+++ Dms/UnitTests/GlobalizationIndependentTestBase.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using NUnit.Framework;
+
+namespace Remotion.Dms.UnitTests
+{
+  public abstract class GlobalizationIndependentTestBase
+  {
+
+    [TestFixtureSetUp]
+    public void TestFixtureSetUp()
+    {
+      var culture = new CultureInfo("en-US");
+      var currentThread = Thread.CurrentThread;
+      currentThread.CurrentUICulture = culture;
+      currentThread.CurrentCulture = culture;
+    }
+  }
+}
