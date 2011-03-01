fe02f5f33f492d50e5ac5a63f447b5e4e3afa882
Andreas Obkircher <andreas.obkircher@rubicon.eu>
2011-03-01 09:07:20 +0000

    added Dms/AssemblyInfoShared.cs and referenced it in all dms projects

--- Dms/AssemblyInfoShared.cs
+++ Dms/AssemblyInfoShared.cs
@@ -1,4 +1,4 @@
-// This file is part of re-vision (www.re-motion.org)
+// This file is part of re-strict (www.re-motion.org)
 // Copyright (C) 2005-2009 rubicon informationstechnologie gmbh, www.rubicon.eu
 // 
 // This program is free software; you can redistribute it and/or modify
@@ -15,11 +15,29 @@
 // 
 // Additional permissions are listed in the file re-motion_exceptions.txt.
 // 
+using System;
 using System.Reflection;
+
 //
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 //
-[assembly: AssemblyTitle("Remotion Dms Library Unit Tests")]
-[assembly: AssemblyCulture("")]		
+[assembly: AssemblyConfiguration (".NET Framework: net-3.5, build type: debug")]
+[assembly: AssemblyCompany("rubicon informationstechnologie gmbh")]
+[assembly: AssemblyProduct ("re-vision (www.re-motion.org)")]
+[assembly: AssemblyCopyright ("(c) 2005 - 2009 rubicon informationstechnologie gmbh, www.rubicon.eu")]
+[assembly: AssemblyTrademark ("")]
+
+//
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers 
+// by using the '*' as shown below:
+
+[assembly: AssemblyVersion("1.13.60.2")]
