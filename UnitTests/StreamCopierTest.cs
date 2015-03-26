a336193c2caba34cd0bee914985d04e835b2fe0e
Michael Ketting <michael.ketting@rubicon.eu>
2015-03-26 15:08:28 +0100

    Switch license from AGPL v3 to LGPL v2.1
    Remotion.IO has been separated from the DMS and is now part of the general purpose re-motion infrastructure, thus the license has been updated accordingly.

--- UnitTests/StreamCopierTest.cs
+++ UnitTests/StreamCopierTest.cs
@@ -1,10 +1,10 @@
-// This file is part of the re-motion Core Framework (www.re-motion.org)
+// This file is part of the re-motion Framework (www.re-motion.org)
 // Copyright (c) rubicon IT GmbH, www.rubicon.eu
 // 
-// The re-motion Core Framework is free software; you can redistribute it 
-// and/or modify it under the terms of the GNU Lesser General Public License 
-// as published by the Free Software Foundation; either version 2.1 of the 
-// License, or (at your option) any later version.
+// re-motion is free software; you can redistribute it and/or modify it under 
+// the terms of the GNU Lesser General Public License as published by the 
+// Free Software Foundation; either version 2.1 of the License, 
+// or (at your option) any later version.
 // 
 // re-motion is distributed in the hope that it will be useful, 
 // but WITHOUT ANY WARRANTY; without even the implied warranty of 
